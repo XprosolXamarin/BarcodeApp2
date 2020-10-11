3d785e6c9ea81aef472fb77e3e99fe787318ede9
Luqman Akram <luqmanakram00300@outlook.com>
2020-10-11 06:35:48 -0700

    Add project files.

---
+++ BarcodeApp2/BarcodeApp2/Models/MainCategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarcodeApp2.Models
+{
+   public class MainCategoryModel
+    {
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ BarcodeApp2/BarcodeApp2/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,72 @@
+﻿using BarcodeApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BarcodeApp2.ViewModels
+{
+    public class DashboardViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MainCategoryModel> Category { get; set; } = new ObservableCollection<MainCategoryModel>();
+
+        public DashboardViewModel()
+        {
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details= "Sunsilk",
+                ImageUrl= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMCUkSK3ANkZaCNV5rymrJKW6J9ZcB3hTK0w&usqp=CAU"
+
+
+            });
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details = "Sunsilk",
+                ImageUrl = "sunsilkshampoo.jpg"
+
+
+            });
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details = "Sunsilk",
+                ImageUrl = "sunsilkshampoo.jpg"
+
+
+            });
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details = "Sunsilk",
+                ImageUrl = "sunsilkshampoo.jpg"
+
+
+            });
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details = "Sunsilk",
+                ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMCUkSK3ANkZaCNV5rymrJKW6J9ZcB3hTK0w&usqp=CAU"
+
+
+            });
+            Category.Add(new MainCategoryModel
+            {
+                Name = "Shampoos",
+                Details = "Sunsilk",
+                ImageUrl = "sunsilkshampoo.jpg"
+
+
+            });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
